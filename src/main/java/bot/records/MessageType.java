fc035d9daa3db07cad8c81b1cb285b48c655eae8
phxgg <stamatis.1702@gmail.com>
2023-10-12 10:20:44 +0300

    update messagetype error color

--- src/main/java/bot/records/MessageType.java
+++ src/main/java/bot/records/MessageType.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public enum MessageType {
     Info(Color.DARK_GRAY),
     Success(new Color(0x2ECC71)), // dark green
-    Error(Color.RED),
+    Error(new Color(0xE74C3C)), // dark red
     Warning(Color.ORANGE),
     TrackBox(Color.PINK);
 
