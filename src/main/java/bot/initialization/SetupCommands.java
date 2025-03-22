7d545639fd55a738e7607d10a98b123c202e7f21
phxgg <45177754+phxgg@users.noreply.github.com>
2025-03-22 03:37:37 +0200

    chore/refactor: bump deps and replace setGuildOnly with setContexts (#20)

--- src/main/java/bot/initialization/SetupCommands.java
+++ src/main/java/bot/initialization/SetupCommands.java
@@ -2,6 +2,7 @@ package bot.initialization;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -13,71 +14,71 @@ public class SetupCommands {
         jda.updateCommands().addCommands(
 //                    Commands.slash("playlocal", "play local track"),
                 Commands.slash("help", "Shows the help embed box.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("play", "Starts playing something.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .addOptions(
                                 new OptionData(OptionType.STRING, "query", "Could be a song, playlist or album name or link. YouTube & Spotify.")
                                         .setRequired(true)),
                 Commands.slash("stop", "Stop player.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("pause", "Pause current playing track. Use again to unpause.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("resume", "Resume player.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("shuffle", "Shuffles the queue.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("skip", "Skips the current song.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("disconnect", "Disconnect player.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("playnow", "Destroys current queue and plays whatever provided.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .addOptions(
                                 new OptionData(OptionType.STRING, "query", "Could be a song, playlist or album name or link. YouTube & Spotify.")
                                         .setRequired(true)),
                 Commands.slash("playnext", "Adds in queue whatever provided right after the current track.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .addOptions(
                                 new OptionData(OptionType.STRING, "query", "Could be a song, playlist or album name or link. YouTube & Spotify.")
                                         .setRequired(true)),
                 Commands.slash("previous", "Plays previous track.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("history", "Shows track history.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("song", "Shows current playing track.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("queue", "Display current queue list.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("clearqueue", "Clears the queue.")
-                        .setGuildOnly(true),
+                        .setContexts(InteractionContextType.GUILD),
                 Commands.slash("volume", "Set the volume player.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .addOptions(
                                 new OptionData(OptionType.INTEGER, "value", "0-100")
                                         .setRequired(true)
                                         .setRequiredRange(0, 100)),
                 Commands.slash("forward", "Forward track by given seconds.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .addOptions(
                                 new OptionData(OptionType.INTEGER, "seconds", "Number of seconds you want to forward.")
                                         .setRequired(true)),
                 Commands.slash("backward", "Backward track by given seconds.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .addOptions(
                                 new OptionData(OptionType.INTEGER, "seconds", "Number of seconds you want to forward.")
                                         .setRequired(true)),
                 Commands.slash("setoutputchannel", "[ADMIN] Set output channel to current message channel.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
                 ,Commands.slash("util", "Utility commands.")
-                        .setGuildOnly(true)
+                        .setContexts(InteractionContextType.GUILD)
                         .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
                         .addSubcommands(
                                 new SubcommandData("context", "Show current guild context.")
                         )
 //                ,Commands.slash("bank", "Banking commands.")
-//                        .setGuildOnly(true)
+//                        .setContexts(InteractionContextType.GUILD)
 //                        .addSubcommands(
 //                                new SubcommandData("balance", "Shows your balance."),
 //                                new SubcommandData("deposit", "Deposit money to your bank.")
